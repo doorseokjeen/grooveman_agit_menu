2de0f08d9feb065b38f240a815987076b88ea0a7
doorseokjeen <74698088+doorseokjeen@users.noreply.github.com>
2021-02-25 16:26:49 +0900

    Add files via upload

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 const redcontent = document.getElementById("redwinemenu");
 const redbtn = document.getElementById("red");
 const whitecontent = document.getElementById("whitewinemenu");
